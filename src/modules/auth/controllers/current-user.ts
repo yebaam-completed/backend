98e821be3195ff66fd60a2202d18b250e45373dc
flowercordoba <flowercordoba7@gmail.com>
2024-09-16 22:49:00 -0500

    added more infor for json of user in the current-user this opcion is tempory

--- src/modules/auth/controllers/current-user.ts
+++ src/modules/auth/controllers/current-user.ts
@@ -25,18 +25,63 @@ export class CurrentUser {
       const cachedUser: IUserDocument = await userCache.getUserFromCache(userId) as IUserDocument;
       const existingUser: IUserDocument = cachedUser || await userService.getUserById(userId);
 
+      // if (existingUser) {
+      //   isUser = true;
+      //   token = req.session?.jwt || null;
+      //   user = {
+      //     _id: existingUser._id,
+      //     username: existingUser.username,
+      //     email: existingUser.email,
+      //     avatarColor: existingUser.avatarColor,
+      //     profilePicture: existingUser.profilePicture,
+
+      //   };
+      // }
       if (existingUser) {
         isUser = true;
         token = req.session?.jwt || null;
         user = {
           _id: existingUser._id,
+          authId: existingUser.authId,
           username: existingUser.username,
           email: existingUser.email,
+          password: existingUser.password,
           avatarColor: existingUser.avatarColor,
+          uId: existingUser.uId,
+          postsCount: existingUser.postsCount,
+          work: existingUser.work,
+          school: existingUser.school,
+          quote: existingUser.quote,
+          infoGeneral: existingUser.infoGeneral,
+          empleo: existingUser.empleo,
+          residendencia: existingUser.residendencia,
+          contact: existingUser.contact,
+          relacion: existingUser.relacion,
+          perfil: existingUser.perfil,
+          acontecimientos: existingUser.acontecimientos,
+          location: existingUser.location,
+          blocked: existingUser.blocked,
+          blockedBy: existingUser.blockedBy,
+          followersCount: existingUser.followersCount,
+          followingCount: existingUser.followingCount,
+          notifications: existingUser.notifications,
+          social: existingUser.social,
+          bgImageVersion: existingUser.bgImageVersion,
+          bgImageId: existingUser.bgImageId,
           profilePicture: existingUser.profilePicture,
-          // Añade otros campos necesarios para el frontend
+          privacy: existingUser.privacy,
+          privacySettings: existingUser.privacySettings,
+          lastActive: existingUser.lastActive,
+          featuredFriends: existingUser.featuredFriends,
+          savedPosts: existingUser.savedPosts,
+          preferences: existingUser.preferences,
+          searchHistory: existingUser.searchHistory,
+          friendRequests: existingUser.friendRequests,
+          friends: existingUser.friends,
+          createdAt: existingUser.createdAt
         };
       }
+      
 
       // Respuesta final
       res.status(HTTP_STATUS.OK).json({ token, isUser, user });
